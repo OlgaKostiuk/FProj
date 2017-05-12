fc50a57aa4f98ee17fb95adab761571b8027bec3
OlgaKostiuk <olga.d.kostiuk@gmail.com>
2017-05-13 00:52:10 +0300

    Edit film. Create, edit genre. Display, add genre for film

---
+++ FProj.Repository/GenreRepository.cs
@@ -0,0 +1,74 @@
+﻿using FProj.Api;
+using FProj.Data;
+using FProj.Repository.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FProj.Repository
+{
+    public class GenreRepository : BaseRepository, IFetch<GenreApi>, ICrud<GenreApi>
+    {
+        public GenreRepository(FProjContext context): base(context)
+        {
+        }
+
+        public GenreApi Default()
+        {
+            return new GenreApi();
+        }
+        public GenreApi Create(GenreApi model)
+        {
+            Genre genreData = ApiToData.GenreApiToData(model);
+            try
+            {
+                _dbContext.Genre.Add(genreData);
+                _dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+            return DataToApi.GenreToApi(genreData);
+        }
+
+        //public void AddFilmToGenre(GenreApi genre, FilmApi film)
+        //{
+        //    Genre genreData = _dbContext.Genre.FirstOrDefault(x => x.Id == genre.Id);
+        //    Film filmData = _dbContext.Film.FirstOrDefault(x => x.Id == film.Id);
+        //    if (genreData == null || filmData == null) return;
+        //    genreData.Films.Add(filmData);
+        //    _dbContext.SaveChanges();
+        //}
+
+        public void Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<GenreApi> GetAll(bool IsDeleted = false)
+        {
+            return _dbContext.Genre.ToList().Select(x => DataToApi.GenreToApi(x)).ToList();
+        }
+
+        public GenreApi GetById(int Id)
+        {
+            Genre genreData = _dbContext.Genre.FirstOrDefault(x => x.Id == Id);
+            if (genreData == null) return null;
+            return DataToApi.GenreToApi(genreData);
+        }
+
+        public GenreApi Update(GenreApi model)
+        {
+            Genre original = _dbContext.Genre.FirstOrDefault(x => x.Id == model.Id);
+            if (original == null) return null;
+            Genre newData = ApiToData.GenreApiToData(model);
+            _dbContext.Entry(original).CurrentValues.SetValues(newData);
+            _dbContext.SaveChanges();
+            return DataToApi.GenreToApi(original);
+        }
+    }
+}
---
+++ FProj.Web/Controllers/GenreController.cs
@@ -0,0 +1,38 @@
+﻿using FProj.Api;
+using FProj.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FProj.Web.Controllers
+{
+    public class GenreController : Controller
+    {
+        // GET: Genre
+        public ActionResult Create()
+        {
+            ViewBag.Title = "Create";
+            return View(UnitOfWork.Instance.GenreRepository.Default());
+        }
+
+        [HttpPost]
+        public ActionResult Create(GenreApi model)
+        {
+            return Json(UnitOfWork.Instance.GenreRepository.Create(model));
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            ViewBag.Title = "Edit";
+            return View("Create", UnitOfWork.Instance.GenreRepository.GetById(Id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(GenreApi model)
+        {
+            return Json(UnitOfWork.Instance.GenreRepository.Update(model));
+        }
+    }
+}
